fecbef4d9c78e4900af7e1156a39b7e70371d99e
falseinfinity <alexjgraphics@gmail.com>
2019-07-26 00:16:33 -0400

    Update serializer to include default img

--- app/serializers/character_serializer.rb
+++ app/serializers/character_serializer.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 class CharacterSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :name, :movie, :born_on, :location, :likes, :img
+  attributes :id, :first_name, :last_name, :name, :movie, :born_on, :location, :likes, :img_working
 
   def name
     object.first_name.capitalize + ' ' + object.last_name.capitalize
+  end
+
+  def img_working
+    if object.img.length < 5
+      'https://i.imgur.com/X2rjLGk.png'
+    else
+      object.img
+    end
   end
 
   # :editable
